732ba47a8883b7080599443549435cc525f01612
wzz <13731160065@users.noreply.github.com>
2018-06-08 14:20:40 +0800

    ff

--- WZZSecretDemo/WZZSecretDemo/main.m
+++ WZZSecretDemo/WZZSecretDemo/main.m
@@ -67,12 +67,72 @@ void wzzSHA256() {
     NSLog(@"sha256:%@", outStr);
 }
 
+#pragma mark - 辅助方法
+/**
+ 字典序排序
+ 
+ @param dic 字典
+ @return 排序后的key数组
+ */
+NSArray * keySortDic(NSDictionary * dic) {
+    //字典序排序
+    NSMutableArray * keySortArr = [NSMutableArray arrayWithArray:[dic allKeys]];
+    for (int i = 0; i < keySortArr.count-1; i++) {
+        for (int j = 0; j < keySortArr.count-i-1; j++) {
+            if (strcmp([keySortArr[j] UTF8String], [keySortArr[j+1] UTF8String]) > 0) {
+                [keySortArr exchangeObjectAtIndex:j withObjectAtIndex:j+1];
+            }
+        }
+    }
+    return keySortArr;
+}
+
+/**
+ 字典序排序并以keyvalue拼接
+ 
+ @param dic 字典
+ @return 参数拼接
+ */
+NSString * argsToStr(NSDictionary * dic, NSArray * keySortArr) {
+    //拼接参数
+    NSMutableArray * argsArr = [NSMutableArray array];
+    for (int i = 0; i < keySortArr.count; i++) {
+        NSString * key = keySortArr[i];
+        NSString * value = dic[key];
+        [argsArr addObject:[NSString stringWithFormat:@"%@=%@", key, value]];
+    }
+    NSString * argsStr = [argsArr componentsJoinedByString:@"&"];
+    return argsStr;
+}
+
+//对象转json
+NSString * jsonFromObject(id obj) {
+    if (obj == nil) {
+        return nil;
+    }
+    NSError * err;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:obj options:0 error:&err];
+    if(err) {
+        NSLog(@"json解析失败：%@",err);
+        return nil;
+    }
+    return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-//        NSString * str = [WZZSecret SHA1WithString:@"ssss"];
-//        NSLog(@"%@", str);
-        wzzSHA256();
-        wzzAES128();
+        NSMutableDictionary * sha1Dic = [NSMutableDictionary dictionaryWithDictionary:@{@"a":@"A", @"h":@"好", @"3":@"f3"}];
+        NSArray * keySortArr = keySortDic(sha1Dic);//字典序key
+        NSString * argsStr = argsToStr(sha1Dic, keySortArr);//拼接keyvalue
+        NSString * signOrgStr = [argsStr stringByAppendingString:@""];//拼接密钥
+        NSString * sha1 = [WZZSecret SHA1WithString:signOrgStr];
+        NSLog(@"\n>sha1:\n>%@\n>%@", signOrgStr, sha1);
+        
+        sha1Dic[@"sign"] = sha1;
+        
+        NSString * json = jsonFromObject(sha1Dic);
+        NSString * aes = [[WZZSecret AES128EncryptWithString:json key:@""] lowercaseString];
+        NSLog(@"\n>aes:\n>%@\n>%@", json, aes);
     }
     return 0;
 }
